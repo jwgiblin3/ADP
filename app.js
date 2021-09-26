460b2dadd44d49f7b178129aca1e290fa17b3f0f
John Giblin <jwgiblin3@hotmail.com>
2021-09-26 15:31:51 -0400

    Add Linting for AIRBNB

--- app.js
+++ app.js
@@ -1,70 +1,61 @@
-import  fetch  from 'node-fetch'
+import fetch from 'node-fetch';
 
 const getTaskURL = 'https://interview.adpeai.com/api/v1/get-task';
 const submitTaskURL = 'https://interview.adpeai.com/api/v1/submit-task';
 
-(async function() {
-    const response = await getTask();
-
-})();
+getTask();
 
 async function getTask() {
-    const response = await fetch(getTaskURL)
-    .then(response => response.json())
-    .then(data =>
-    {
-        // Run the task
-        const results = runTask(data.operation, data.left, data.right);
+  await fetch(getTaskURL)
+    .then((response) => response.json())
+    .then((data) => {
+      // Run the task
+      const results = runTask(data.operation, data.left, data.right);
 
-        // submit task results
-        postResults(data.id, results);
-
+      // submit task results
+      postResults(data.id, results);
     });
-
 }
 
 function postResults(id, results) {
-    const post  = {
-        id: id,
-        result: results
-    }
+  const post = {
+    id,
+    result: results,
+  };
 
-    fetch(submitTaskURL, {
-        method: 'post',
-        body:    JSON.stringify(post),
-        headers: { 'Content-Type': 'application/json' },
-        }).then(checkStatus)
-        .then(status => {
-            // Return submitted task results
-            if (status) {
-                console.log('Task was successfully submitted');
-            } else {
-                console.log('Task was NOT successfully submitted');
-            }
-        });
+  fetch(submitTaskURL, {
+    method: 'post',
+    body: JSON.stringify(post),
+    headers: { 'Content-Type': 'application/json' },
+  })
+    .then(checkStatus);
 }
 
 function runTask(operation, left, right) {
-
-    if ( operation === 'addition') {
-        return left + right;
-    } else if ( operation === 'subtraction') {
-        return left - right;
-    } else if ( operation === 'multiplication') {
-        return left*right;
-    } else if ( operation === 'division') {
-        return left/right;
-    } else if ( operation === 'divissubtractionion') {
-        return left - right;
-    } else if ( operation === 'remainder') {
-        return left % right;
-    }
-    return -1;
+  if (operation === 'addition') {
+    return left + right;
+  } if (operation === 'subtraction') {
+    return left - right;
+  } if (operation === 'multiplication') {
+    return left * right;
+  } if (operation === 'division') {
+    return left / right;
+  } if (operation === 'divissubtractionion') {
+    return left - right;
+  } if (operation === 'remainder') {
+    return left % right;
+  }
+  return -1;
 }
+
 function checkStatus(res) {
-    if(res.ok) {
-        return true;
-    } else {
-        return false;
-    }
+  if (res.status === 200) {
+    console.info('Task was successfully submitted');
+  } else if (res.status === 400) {
+    console.error('Task was unsuccessfully: Incorrect value in result; no ID specified; value is invalid');
+  } else if (res.status === 500) {
+    console.error('Task was unsuccessfully: ID cannot be found');
+  } else {
+    console.error('Task was unsuccessfully: Unknow error');
+  }
 }
